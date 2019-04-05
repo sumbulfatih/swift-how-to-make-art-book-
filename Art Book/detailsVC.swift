cd7db4bee09a49e688c12150a0f43dde607edcea
SUMBUL <>
2019-04-06 01:10:37 +0300

    art book

---
+++ Art Book/detailsVC.swift
@@ -0,0 +1,136 @@
+//
+//  detailsVC.swift
+//  Art Book
+//
+///  Created by SUMBUL on 6.04.2019.
+//  Copyright © 2019 SUMBUL. All rights reserved.
+//
+
+
+import UIKit
+import CoreData
+
+class detailsVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var nameText: UITextField!
+    @IBOutlet weak var artistText: UITextField!
+    @IBOutlet weak var yearText: UITextField!
+    var chosenPainting = ""
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if chosenPainting != "" {
+            
+            let appDelegate = UIApplication.shared.delegate as! AppDelegate
+            let context = appDelegate.persistentContainer.viewContext
+            
+            let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Paintings")
+            
+            fetchRequest.predicate = NSPredicate(format: "name = %@", self.chosenPainting)
+            fetchRequest.returnsObjectsAsFaults = false
+            
+            
+            do {
+                
+                let results = try context.fetch(fetchRequest)
+                
+                if results.count > 0 {
+                    
+                    for result in results as! [NSManagedObject] {
+                        
+                        if let name = result.value(forKey: "name") as? String {
+                            nameText.text = name
+                        }
+                        
+                        if let year = result.value(forKey: "year") as? Int {
+                            yearText.text = String(year)
+                        }
+                        
+                        if let artist = result.value(forKey: "artist") as? String {
+                            artistText.text = artist
+                        }
+                        
+                        if let imageData = result.value(forKey: "image") as? Data {
+                            let image = UIImage(data: imageData)
+                            self.imageView.image = image
+                        }
+                        
+                    }
+                    
+                    
+                }
+                
+            } catch {
+                print("error")
+            }
+            
+            
+        }
+
+        imageView.isUserInteractionEnabled = true
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(detailsVC.selectImage))
+        imageView.addGestureRecognizer(gestureRecognizer)
+        
+    }
+    
+    
+    @objc func selectImage() {
+        
+        let picker = UIImagePickerController()
+        picker.delegate = self
+        picker.sourceType = .photoLibrary
+        picker.allowsEditing = true
+        present(picker, animated: true, completion: nil)
+        
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        //swift 4
+        //imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
+
+        imageView.image = info[.originalImage] as? UIImage
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func saveButtonClicked(_ sender: Any) {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let newArt = NSEntityDescription.insertNewObject(forEntityName: "Paintings", into: context)
+        
+        //attributes
+        newArt.setValue(nameText.text, forKey: "name")
+        newArt.setValue(artistText.text, forKey: "artist")
+        
+        if let year = Int(yearText.text!) {
+            newArt.setValue(year, forKey: "year")
+        }
+        
+        //swift4
+        // let data = UIImageJPEGRepresentation(imageView.image!, 0.5)
+        
+        //swift 4.2
+        let data = imageView.image!.jpegData(compressionQuality: 0.5)
+        newArt.setValue(data, forKey: "image")
+        
+        do {
+            try context.save()
+            print("succesful")
+        } catch {
+            print("error")
+        }
+        
+        NotificationCenter.default.post(name: NSNotification.Name(rawValue: "newPainting"), object: nil)
+        self.navigationController?.popViewController(animated: true)
+        
+        
+    }
+    
+}
+
+
---
+++ README.md
@@ -0,0 +1,1 @@
+# swift-how-to-make-art-book-
